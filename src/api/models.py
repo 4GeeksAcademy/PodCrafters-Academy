93877eff4b2abab072f76c7ce8fe0669a8c5237f
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2024-02-28 20:25:34 +0000

    User terminado

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    userName = db.Column(db.String(80), unique=True, nullable=False)
+    firstName = db.Column(db.String(80), unique=False, nullable=False)
+    lastName = db.Column(db.String(80), unique=False, nullable=False)
+    birthDay = db.Column(db.String(80), unique=False, nullable=False)
+    gender = db.Column(db.Enum("masculino","femenino","no_binario"), unique=False, nullable=False)
+    id_profile_picture = db.Column(db.Integer, unique=True, nullable=True)
+    profesor = db.Column(db.Boolean, nullable=False)
+    fecha_registro = db.Column(db.String(80), unique=False, nullable=False)
+    telephone = db.Column(db.String(20), unique=True, nullable=False)
+    country = db.Column(db.String(40), unique=False, nullable=False)
+    address = db.Column(db.String(150), unique=False, nullable=False)
+    idioma_español = db.Column(db.Boolean, nullable=False )
+    ultima_conexion = db.Column(db.String(40), nullable=False)
+    mentor = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
