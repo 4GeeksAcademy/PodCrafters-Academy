f012d4984c1ddabd0b9f9bbc641a67490f0be1dc
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2024-03-01 21:58:56 +0000

    first commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#E2F4F4" }}>
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src="https://raw.githubusercontent.com/4GeeksAcademy/PodCrafters-Academy/26da7e58178507f3f07f6acd7b1bc9a6082dd9bf/_0fd8546c-634c-4803-9f7b-78b166b7b7bb.jpg" alt="Logo" height="100" />
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{ color: "#000", background: "#E5E5E5", borderRadius: "5px", padding: "5px 10px", margin: "0 5px"  }}>Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{ color: "#000", background: "#E5E5E5", borderRadius: "5px", padding: "5px 10px", margin: "0 5px"  }}>Log In / Registrar</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{ color: "#000", background: "#E5E5E5", borderRadius: "5px", padding: "5px 10px", margin: "0 5px" }}>Cursos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{ color: "#000", background: "#E5E5E5", borderRadius: "5px", padding: "5px 10px", margin: "0 5px"  }}>Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
