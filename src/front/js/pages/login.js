faf9557ee80b4f29afad1b6353fc8f80f04016e7
Chogyal Bhutia <61793948+cbhutia@users.noreply.github.com>
2024-03-11 17:45:17 +0000

    .

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom"; // Import Link
 import "../../styles/login.css"; // Updated CSS file import
 
 export const Login = () => {
@@ -15,36 +15,46 @@ export const Login = () => {
     }
 
     return (
-        <div className="login-container">
+        <div className="container">
             <h1 className="login-title">Inicia Sesión</h1>
-            <form onSubmit={handleLoginSubmit} className="login-form">
-                <div className="form-group">
-                    <input
-                        type="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="login-input"
-                        id="email"
-                        placeholder="Email address"
-                        required
-                    />
+            <div className="login">
+                <div className="login-container">
+                    <form onSubmit={handleLoginSubmit} className="login-form">
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="login-input"
+                                id="email"
+                                placeholder="Correo electronico"
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                name="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="login-input"
+                                id="password"
+                                placeholder="Contraseña"
+                                required
+                            />
+                        </div>
+                        <button type="submit" className="login-submit">Login</button>
+                        <a href="#" className="forgot-password">¿Has olvidado tu contraseña?</a>
+
+                    </form>
+
                 </div>
-                <div className="form-group">
-                    <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="login-input"
-                        id="password"
-                        placeholder="Password"
-                        required
-                    />
+                <div className="signupContainer">
+                    <h1 className="signup-title">Regístrate</h1>
+                    <p>Si todavía no tienes una cuenta, puedes <Link to="/signup" className="signup-link">registrarte aquí</Link>.</p>
                 </div>
-                <button type="submit" className="login-submit">Login</button>
-            </form>
-            <a href="#" className="forgot-password">Forgot password?</a>
+            </div>
         </div>
     );
 };
